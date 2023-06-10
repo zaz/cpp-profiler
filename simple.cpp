4ccea71b645e96d34e06906d3b9de64999fbcd2e
Zaz Brown <zazbrown@zazbrown.com>
2023-06-10 17:53:12 +0000

    simplify simple.cpp for brevity

--- simple.cpp
+++ simple.cpp
@@ -1,11 +1,3 @@
-////////////////////////////////////////////////////////////////////
-// File:         simple.cpp
-// Creation:     4/2013
-// Programmer:   Dr. J. Maletic
-//
-// Description:  Simple program for testing profiler
-//
-//
 #include <iostream>
 
 int search(int tbl[], int n, int key) {
@@ -18,10 +10,7 @@ int search(int tbl[], int n, int key) {
     return result;
 }
 
-
-
 int main() {
-
     int lst[5] = {2, 4, 6, 8, 10};
     std::cout << search(lst, 5, 6);
     std::cout << std::endl;
